f7764646eed5e1860ddc24bf8386d93df79257e8
trq <trq>
1995-03-02 23:13:13 +0000

    Inserted definitions into heatcool.c.

--- code/heatcool.c
+++ code/heatcool.c
@@ -576,7 +576,24 @@ double midpnt(func,a,b,n)
 
 #include <math.h>
 #define NRANSI
-#include "nrutil.h"
+
+double *vector_nr();
+double **matrix();
+double **convert_matrix();
+double *dvector_nr();
+double **dmatrix();
+int *ivector_nr();
+int **imatrix();
+double **submatrix();
+void free_vector_nr();
+void free_dvector_nr();
+void free_ivector_nr();
+void free_matrix();
+void free_dmatrix();
+void free_imatrix();
+void free_submatrix();
+void free_convert_matrix();
+void nrerror();
 
 void polint(xa,ya,n,x,y,dy)
         double xa[],ya[],x,*y,*dy ;
