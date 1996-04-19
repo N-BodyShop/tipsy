2bb8a32289077c3d4549c0172a2fec68a774c93f
trq <trq>
1996-04-19 19:29:46 +0000

    Bumped version number.

--- code/version.c
+++ code/version.c
@@ -1,11 +1,11 @@
 #include "defs.h"
 
-static char version_string[] = "2.1.7";
+static char version_string[] = "2.1.8";
 
 void
 version(job)
     char job[MAXCOMM] ;
 {
-    printf("version %s:  27 DECEMBER 1995\n", version_string) ;
+    printf("version %s:  19 APRIL 1996\n", version_string) ;
     printf("MAXBOX = %d\n", MAXBOX) ;
 }
