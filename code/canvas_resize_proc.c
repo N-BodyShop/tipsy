585c3dd9ed0bb140d0d4aa332e58f1ea1a72e40a
trq <trq>
1998-02-18 22:51:21 +0000

    Avoid extraneous redraw messages from plotsub().

--- code/canvas_resize_proc.c
+++ code/canvas_resize_proc.c
@@ -1,7 +1,10 @@
 /* $Header$
  * $Log$
- * Revision 1.1  1995/01/10 22:57:30  trq
- * Initial revision
+ * Revision 1.2  1998/02/18 22:51:21  trq
+ * Avoid extraneous redraw messages from plotsub().
+ *
+ * Revision 1.1.1.1  1995/01/10  22:57:31  trq
+ * Import to CVS
  *
  * Revision 2.4  1994/12/29  02:26:49  trq
  * changed can_size from min to max of height and width.
@@ -29,6 +32,9 @@ canvas_resize_proc(canvas,event, params, n_params)
     XtSetArg(args[1], XtNheight, &can_height);
     XtGetValues(canvas, args, 2);
     can_size = max(can_height,can_width) ;
+    if(!current_project || !current_color)
+	return;
+    
     plot_sub("redraw");
     draw_label();
 }
