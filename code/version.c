1479e34af29967af350307008e3238a2ff162ebb
trq <trq>
1999-08-31 16:07:44 +0000

    Version 2.2.

--- code/version.c
+++ code/version.c
@@ -1,11 +1,11 @@
 #include "defs.h"
 
-static char version_string[] = "2.1.11";
+static char version_string[] = "2.2";
 
 void
 version(job)
     char job[MAXCOMM] ;
 {
-    printf("version %s:  25 JULY 1997\n", version_string) ;
+    printf("version %s:  31 AUGUST 1999\n", version_string) ;
     printf("MAXBOX = %d\n", MAXBOX) ;
 }
