870ab02d275508affab11baef26d1b236a631b6a
nsk <nsk>
2000-07-07 03:32:03 +0000

    Print variables and arbitrary text to screen (like unix echo).

---
+++ code/print.c
@@ -0,0 +1,13 @@
+#include "defs.h"
+
+/* print the arguments -- most commonly used to print values of variables 
+   actual variable substitution is handled elsewhere */
+void print(char job[])
+{
+  int nread;
+  char junk[MAXVAR], rest[MAXCOMM];
+
+  nread = sscanf(job, "%s %[^\n]", junk, rest);
+  if (nread == 2) printf("\n%s\n\n", rest);
+
+}  
