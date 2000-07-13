3b59e3d62d5f30435ff39394f52403c65e439498
nsk <nsk>
2000-07-13 01:18:15 +0000

    New command: close index of single-timeslice files.

---
+++ code/closemanifest.c
@@ -0,0 +1,75 @@
+
+#include "defs.h"
+#include <malloc.h>
+#include <stdlib.h>
+
+void
+closemanifest(job)
+    char *job ;
+{
+    if (!binaryopen || !ismanifest){
+	printf("<sorry, manifest file not open, %s>\n",title) ;
+    }
+    else {
+	fclose(binaryfile.ptr);
+	free(manifest);
+	manifest = NULL;
+	binaryopen = CLOSED ;
+	binary_loaded = UNLOADED ;
+        if(mark_gas != NULL) {
+ 	   free(mark_gas);
+           mark_gas = NULL;
+	}
+        if(mark_dark != NULL) {
+ 	   free(mark_dark);
+           mark_dark = NULL;
+	}
+        if(mark_star != NULL) {
+ 	   free(mark_star);
+           mark_star = NULL;
+	}
+        if(hsmdivv != NULL) {
+ 	   free(hsmdivv);
+           hsmdivv = NULL;
+	   divv_loaded = NO ;
+	}
+        if(cooling != NULL) {
+ 	   free(cooling);
+           cooling = NULL;
+	   cooling_loaded = NO ;
+	}
+        if(hneutral != NULL) {
+ 	   free(hneutral);
+           hneutral = NULL;
+	   hneutral_loaded = NO ;
+	}
+        if(heneutral != NULL) {
+ 	   free(heneutral);
+           heneutral = NULL;
+	}
+        if(heII != NULL) {
+ 	   free(heII);
+           heII = NULL;
+	}
+        if(meanmwt != NULL) {
+ 	   free(meanmwt);
+           meanmwt = NULL;
+	   meanmwt_loaded = NO ;
+	}
+        if(starform != NULL) {
+ 	   free(starform);
+           starform = NULL;
+	   starform_loaded = NO ;
+	}
+        if(xray_lum != NULL) {
+ 	   free(xray_lum);
+           xray_lum = NULL;
+	   xray_loaded = NO ;
+	}
+	if(dudt != NULL) {
+	   free(dudt) ;
+	   dudt = NULL ;
+	   dudt_loaded = NO ;
+	}
+    }
+}
