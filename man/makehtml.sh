85b986b25b5ada1ea21d6031cac0b8ab371dacdb
trq <trq>
2003-08-28 16:09:14 +0000

    Page formating tweeks.

--- man/makehtml.sh
+++ man/makehtml.sh
@@ -2,5 +2,5 @@
 cd man1
 for i in *.1 ; do
   page=`basename $i .1`
-  man -M .. $page | ../../../man2html3.0.1/man2html -pgsize 1000 -cgiurl \$title.html > ../html1/$page.html
+  man -M .. $page | ../../../man2html3.0.1/man2html -topm 0 -botm 0 -cgiurl \$title.html > ../html1/$page.html
 done
