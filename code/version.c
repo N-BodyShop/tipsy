31812b4a5d5f84f5549eb922f25e883d1fa54ebc
trq <trq>
2007-08-02 19:28:37 +0000

    Put up a new version on the FTP site.

--- code/version.c
+++ code/version.c
@@ -1,11 +1,11 @@
 #include "defs.h"
 
-static char version_string[] = "2.2.3a";
+static char version_string[] = "2.2.3b";
 
 void
 version(job)
     char job[MAXCOMM] ;
 {
-    printf("version %s:  09 AUG 2006\n", version_string) ;
+    printf("version %s:  03 MAR 2007\n", version_string) ;
     printf("MAXBOX = %d\n", MAXBOX) ;
 }
